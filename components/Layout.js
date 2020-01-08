6956c03b607f2d30b338a44081ae2130f1a51ee5
Andy Nyau <andynyau@gmail.com>
2020-01-09 01:06:00 +1030

    fixed image folder reference - again

--- components/Layout.js
+++ components/Layout.js
@@ -26,11 +26,11 @@ const Layout = props => (
             <meta name="apple-mobile-web-app-capable" content="yes" />
             <meta name="apple-mobile-web-app-status-bar-style" content="black" />
 
-            <link rel="apple-touch-icon" sizes="180x180" href="/content/images/apple-touch-icon.png" />
-            <link rel="icon" type="image/png" sizes="32x32" href="/content/images/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/content/images/favicon-16x16.png" />
-            <link rel="manifest" href="/content/images/site.webmanifest" />
-            <link rel="mask-icon" href="/content/images/safari-pinned-tab.svg" color="#5bbad5" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+            <link rel="manifest" href="/images/site.webmanifest" />
+            <link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#5bbad5" />
             <meta name="msapplication-TileColor" content="#da532c" />
             <meta name="theme-color" content="#ffffff" />
 
