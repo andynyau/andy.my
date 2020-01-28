65ef910af47f811baf380a50749116925d003d80
Andy Nyau <andy.nyau@ttonline.com.au>
2020-01-28 15:40:28 +1030

    compile after fix for mobile layout background

--- docs/_next/static/runtime/webpack.js
+++ docs/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "047c630b8ab0a00b01b4";
+/******/ 	var hotCurrentHash = "6c961ff0bbd30bf62723";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
